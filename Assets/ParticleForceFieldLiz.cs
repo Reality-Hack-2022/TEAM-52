4cfb914a85e9448b47d0ffc4ed5f6611049d1bcd
Liz Newton-Tanzer <54453455+lizecaint@users.noreply.github.com>
2022-03-26 19:51:50 -0400

    bug fix:)

--- Assets/ParticleForceFieldLiz.cs
+++ Assets/ParticleForceFieldLiz.cs
@@ -20,7 +20,6 @@ public class ParticleForceFieldLiz : MonoBehaviour
     public HandModelBase m_LefthandModeBase;
     public HandModelBase m_RighthandModeBase;
 
-
     public InteractionHand m_LeftHand;
     public InteractionHand m_RightHand;
 
@@ -31,14 +30,17 @@ public class ParticleForceFieldLiz : MonoBehaviour
 
     public PostProcessLayer postProcessLayer;
 
+    public bool PlayedOnce = false;
+
+    public float timerWait;
+    public float timerVideo;
+
 
     public void SetPostProcessingLayerIsEnabled(bool _value)
     {
         if (postProcessLayer == null) return;
         postProcessLayer.enabled = _value;
     }
-
-
 
     private void Start()
     {
@@ -60,81 +62,85 @@ public class ParticleForceFieldLiz : MonoBehaviour
 
         m_RighthandModeBase.OnFinish -= EndForceField;
         m_RighthandModeBase.OnFinish += EndForceField;
-    }
 
+        timerVideo = -1;
+        timerWait = -1;
+    }
 
     public void StartForceField()
     {
         m_forceField.gameObject.SetActive(true);
         m_IsHandActive = true;
-        StartCoroutine(Wait());
-        
+        if(!m_Video.activeSelf)
+        {
+            timerWait = 4;
+        }
 
-        
     }
     public void EndForceField()
     {
         m_forceField.gameObject.SetActive(false);
         m_IsHandActive = false;
-
+        timerWait = -1;
     }
 
     private void Update()
     {
-        //if (m_forceField.gameObject.activeSelf)
         if (m_forceField.gameObject.activeSelf)
         {
             if (m_RightHand._hand != null)
             {
                 Vector3 palmPos = m_RightHand._hand.PalmPosition.ToVector3();
-                this.transform.position = palmPos;
+                m_forceField.transform.position = palmPos;
                 visualEffect.SetVector3(palmID, palmPos);
             }
             else if (m_LeftHand._hand != null)
             {
                 Vector3 palmPos = m_LeftHand._hand.PalmPosition.ToVector3();
-                this.transform.position = palmPos;
+                m_forceField.transform.position = palmPos;
                 visualEffect.SetVector3(palmID, palmPos);
             }
-
         }
+
+        if (timerWait > 0)
+        {
+            timerWait -= Time.deltaTime;
+        }
+        else if (timerWait != -1)
+        {
+            WaitEnd();
+            timerWait = -1;
+            timerVideo = 6;
+        }
+
+        if(timerVideo>0)
+        {
+            timerVideo -= Time.deltaTime;
+        }
+        else if (timerVideo != -1)
+        {
+            VideoEnd();
+            timerVideo = -1;
+            if(m_IsHandActive)
+            {
+                timerWait = 4;
+            }
+        }
+
     }
 
-    private IEnumerator Wait()
+    private void WaitEnd()
     {
-        if(!m_IsHandActive)
-        {
-            StopAllCoroutines();
-        }
-        while (m_IsHandActive)
-        {
-            yield return new WaitForSeconds(4);
             m_Video.SetActive(true);
-           
             m_particleSystem.Stop();
-            SetPostProcessingLayerIsEnabled(true);
-            StartCoroutine(VideoTimer());
-
-        }
-
     }
 
-    public void videoEnded()
+    private void VideoEnd()
     {
-        Debug.Log("videoEnded() triggered");
-        //m_particleSystem.Play();
-        //m_Video.SetActive(false);
+        m_Video.SetActive(false);
+        m_particleSystem.Play();
+        SetPostProcessingLayerIsEnabled(false);
 
-    }
 
-    private IEnumerator VideoTimer()
-    {
-        while (m_IsHandActive)
-        {
-            yield return new WaitForSeconds(6);
-            m_Video.SetActive(false);
-            m_particleSystem.Play();
-            SetPostProcessingLayerIsEnabled(false);
-        }
     }
 }
